87e0db14ee5a79781fde396a5636a7b5609173a7
William Choi <wchoi95@gmail.com>
2018-03-13 14:40:52 -0700

    made git ignore and basic web client/server

---
+++ web/server/constants/routes.js
@@ -0,0 +1,4 @@
+  // React router needs to load from the index in order to route properly.
+  // Send the index file from any routes on our site. These routes need to be
+  // expressed stated here.
+export const ROUTES = ['/'];
