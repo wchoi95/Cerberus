80039158042518a457b7ce6676e6c3687aac8ab9
Ben <ben.harder2@gmail.com>
2018-04-03 01:03:12 -0700

    App done (I think)

--- CerberusApp/Cerberus/platforms/browser/www/cordova_plugins.js
+++ CerberusApp/Cerberus/platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,27 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/browser/push.js",
+        "id": "phonegap-plugin-push.BrowserPush",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "phonegap-plugin-push": "2.2.2"
 }
 // BOTTOM OF METADATA
 });
